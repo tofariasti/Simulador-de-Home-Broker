db24fb8b15e812d00a9c5fcee224d4d98ff70b8d
Tiago O. de Farias <131496768+tofariasti@users.noreply.github.com>
2026-03-15 19:05:21 +0000

    Add - print marketData

--- src/main/java/com/example/HomeBroker.java
+++ src/main/java/com/example/HomeBroker.java
@@ -2,7 +2,23 @@ package com.example;
 
 public class HomeBroker {
     public static void main(String[] args) {
+
+        String[][] maketData = new String[5][3];
+        int colStockSymbol = 0;
+        int colCurrentPrice = 1;
+        int colAvailableQuantity = 2;
+
+        printMarketData(maketData);
+
         System.out.println("ASD! Simulador de Home Broker iniciado!");
         System.out.println("Use comandos de terminal para interagir.");
+    }
+
+    public static void printMarketData(String[][] marketData) {
+        System.out.println("Mercado Atual:");
+        System.out.println("Símbolo | Preço Atual | Quantidade Disponível");
+        for (String[] stock : marketData) {
+            System.out.printf("%s | %s | %s%n", stock[0], stock[1], stock[2]);
+        }
     }
 }
