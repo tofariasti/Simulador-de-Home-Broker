91b9a4c1d4ce114af1d15d9ab3f3200bbecfc9f9
Tiago O. de Farias <131496768+tofariasti@users.noreply.github.com>
2026-03-15 18:59:53 +0000

    Add - artifacts

---
+++ src/main/java/com/example/HomeBroker.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HomeBroker {
+    public static void main(String[] args) {
+        System.out.println("ASD! Simulador de Home Broker iniciado!");
+        System.out.println("Use comandos de terminal para interagir.");
+    }
+}
